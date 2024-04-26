ba76e3bb3b4285960443f15a6957800a5ba223a9
adzy404 <adam.isitmez@hotmail.com>
2024-04-26 21:27:21 +1000

    updated title styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
     return (
         <div className="home bebas-neue-h1">
-            <div className="title">
+            <div className="home-title">
                 <h1>Adam Isitmez</h1>
                 <p>Fullstack Web Developer</p>
             </div>
