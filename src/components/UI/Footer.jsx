6230926a05e85122ccfd83de537d5fde5c2dc0e0
adzy404 <adam.isitmez@hotmail.com>
2024-04-28 21:39:14 +1000

    fixed image location

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
     return (
         <div className="footer">
             <div className="icon-list">
-                <a href="https://github.com/AdamIsitmez" target="_blank"><img className="footer-logo" src="src/assets/github-mark.svg" alt="" /></a>
-                <a href="https://www.linkedin.com/in/adam-isitmez-723a86295/" target="_blank"><img className="footer-logo" src="src/assets/linkedin-logo.svg" alt="" /></a>
+                <a href="https://github.com/AdamIsitmez" target="_blank"><img className="footer-logo" src="/github-mark.svg" alt="" /></a>
+                <a href="https://www.linkedin.com/in/adam-isitmez-723a86295/" target="_blank"><img className="footer-logo" src="/linkedin-logo.svg" alt="" /></a>
             </div>
         </div>
     );
